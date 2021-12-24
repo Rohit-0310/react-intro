933a5211a2c1f478bb43cc1481affa2dc98b5175
ROHIT KUMAR <rohit9909mr@gmail.com>
2021-12-24 09:07:51 +0530

    Updated Assignment

--- src/App.js
+++ src/App.js
@@ -2,22 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+    const mobile_os = ["Android", "Blackberry", "iPhone","Windows Phone"];
+    const mobile_mf = ["Samsung","HTC", "Mickromax", "Apple"]
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{
+        color: "black",
+        fontSize: "15px",
+        textAlign: "left",
+        paddingLeft: "30px",
+    }}
+    >
+        <h1 style={{
+            color: "black",
+            fontSize: "40px",
+            
+        }}
+        >Mobile Operating System</h1>
+        {mobile_os.map((e) => (
+            <li style={{
+                color: "black",
+                fontSize: "15px",
+                paddingLeft: "30px",
+            }}
+            >{e}</li>
+        ))}
+        
+        <h1 style={{
+            color: "black",
+            fontSize: "40px",
+        }}
+        >Mobile Manufacturers</h1>
+        {mobile_mf.map((e) => (
+            <li style={{
+                color: "black",
+                fontSize: "15px",
+                paddingLeft: "30px",                
+            }}
+            >{e}</li>
+        ))}
+        
     </div>
   );
 }
